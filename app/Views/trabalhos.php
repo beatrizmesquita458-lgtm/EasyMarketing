cc7b645885796a3b01d897e6f8c9e573e2a596d8
beatrizmesquita458-lgtm <beatrizmesquita458@gmail.com>
2026-09-09 10:17:39 -0300

    coloquei a parte da navbar na tela de trabalhos

--- app/Views/trabalhos.php
+++ app/Views/trabalhos.php
@@ -4,9 +4,72 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    
+    <!--link dos bootstraps utilizados :P-->
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Bootstrap Icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
-<body>
-    <div style="width: 100%; height: 100vh; display: flex; justify-content: center; align-items: center;">
-        <h1>Trabalhos</h1>''
+<body style="padding: 0; margin: 0; background-color: #F9F9FF;">
+    <header>
+    <nav class="navbar bg-white border-bottom" style="height: 70px;">
+
+        <div class="container-fluid px-5">
+
+            <!--,mas se tivesse, seria bom colocar logo abaixo nessa mensagem. copiar do Logo -->
+
+            <!-- LOGO -->
+            <span class="text-primary fw-bold fs-5">
+                EasyMarketing
+            </span>
+            
+            <!-- Menu -->
+            <div class="d-flex gap-4 mx-auto">
+
+                <a href="#" class="text-dark text-decoration-none text-center small">
+                    <i class="bi bi-house-fill d-block fs-5"></i>
+                    Home
+                </a>
+
+                <a href="#" class="text-dark text-decoration-none text-center small">
+                    <i class="bi bi-people d-block fs-5"></i>
+                    Network
+                </a>
+
+                <a href="#" class="text-dark text-decoration-none text-center small">
+                    <i class="bi bi-briefcase d-block fs-5"></i>
+                    Jobs
+                </a>
+
+                <a href="#" class="text-dark text-decoration-none text-center small">
+                    <i class="bi bi-chat-square-text d-block fs-5"></i>
+                    Messaging
+                </a>
+
+            </div>
+
+            <!-- Direita -->
+            <div class="d-flex align-items-center gap-4">
+
+                <i class="bi bi-bell fs-5"></i>
+
+                <i class="bi bi-grid-3x3-gap-fill fs-5"></i>
+
+                <i class="bi bi-play-btn fs-5"></i>
+
+                <div class="border-start" style="height: 30px;"></div>
+
+                <div class="rounded-circle bg-secondary-subtle d-flex align-items-center justify-content-center"
+                     style="width: 32px; height: 32px;">
+                    <i class="bi bi-person"></i>
+                </div>
+
+            </div>
+
+        </div>
+    </nav>
+    </header>
 </body>
 </html>
